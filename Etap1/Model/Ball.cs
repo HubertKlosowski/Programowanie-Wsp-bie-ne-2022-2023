2654288879b1aab8580f55bc64ac3880edda7b9a
Hubert Kłosowski <242424@edu.p.lodz.pl>
2023-04-01 14:51:18 +0200

    Kolejna duza zmiana

---
+++ Etap1/Model/Ball.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Model;
+
+public class Ball
+{
+    private int _x = 0;
+    private int _y = 0;
+    private int _radius = 0;
+
+    public Ball(int x, int y, int r)
+    {
+        _x = x;
+        _y = y;
+        _radius = r;
+    }
+}
