65719d7e824b6408222fc7eba2f9bca423ccf9f5
Hubert Kłosowski <242424@edu.p.lodz.pl>
2023-04-06 17:28:02 +0200

    Towarzystkie Kulki

--- Etap1/Model/Ball.cs
+++ Etap1/Model/Ball.cs
@@ -2,16 +2,75 @@
 
 namespace Model;
 
-public class Ball
+public class Ball : INotifyPropertyChanged
 {
-    private int _x = 0;
-    private int _y = 0;
-    private int _radius = 0;
+    private double _x = 0;
+    private double _y = 0;
+    private int _r = 0;
+    private bool _changeX = false;
+    private bool _changeY = false;
 
     public Ball(int x, int y, int r)
     {
         _x = x;
         _y = y;
-        _radius = r;
+        _r = r;
     }
+    
+    public bool changeY
+    {
+        get { return _changeY; }
+        set
+        {
+            _changeY = value;
+            OnPropertyChanged("changeY");
+        }
+    }
+    
+    public bool changeX
+    {
+        get { return _changeX; }
+        set
+        {
+            _changeX = value;
+            OnPropertyChanged("changeX");
+        }
+    }
+    
+    public double X
+    {
+        get { return _x; }
+        set
+        {
+            _x = value;
+            OnPropertyChanged("x");
+        }
+    }
+    
+    public double Y
+    {
+        get { return _y; }
+        set
+        {
+            _y = value;
+            OnPropertyChanged("y");
+        }
+    }
+    
+    public int R
+    {
+        get { return _r; }
+        set
+        {
+            _r = value;
+            OnPropertyChanged("r");
+        }
+    }
+
+    private void OnPropertyChanged(string direction)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(direction));
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
 }
--- Etap1/View/MainWindow.xaml.cs
+++ Etap1/View/MainWindow.xaml.cs
@@ -1,28 +1,195 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using Model;
 
 namespace View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private List<Ball> _balls = new();
+        private DispatcherTimer timer = new();
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GenerateBalls(object sender, RoutedEventArgs e)
+        {
+            int numOfBalls;
+            try
+            {
+                numOfBalls = Int32.Parse(NumberOfBalls.Text);
+            }
+            catch (FormatException)
+            {
+                numOfBalls = 3;
+                NumberOfBalls.Text = "3";
+            }
+            if (numOfBalls >= 2789)
+            {
+                numOfBalls = 3;
+                NumberOfBalls.Text = "3";
+            }
+            Random random = new Random();
+            List<Brush> colors = new List<Brush>
+            {
+                Brushes.Blue,
+                Brushes.Red,
+                Brushes.Green,
+                Brushes.Yellow,
+                Brushes.Purple,
+                Brushes.Orange,
+                Brushes.Brown,
+                Brushes.Black
+            };
+            int size = colors.Count;
+            int r = random.Next(10, 21);
+            for (int i = 0; i < numOfBalls; i++)
+            {
+                int x = random.Next(5, 684 - r);
+                int y = random.Next(5, 411 - r);
+                Ball ball = new Ball(x, y, r);
+                while (!checkInitialCoordinates(ball))
+                {
+                    ball.X = random.Next(5, 684 - r);
+                    ball.Y= random.Next(5, 411 - r);
+                    if (checkInitialCoordinates(ball))
+                    {
+                        break;
+                    }
+                }
+                Ellipse ellipse = new Ellipse
+                {
+                    Width = 2 * r,
+                    Height = 2 * r,
+                    Fill = colors[i % size],
+                };
+                _balls.Add(ball);
+                Canvas.SetLeft(ellipse, x - _balls[i].R);
+                Canvas.SetTop(ellipse, y - _balls[i].R);
+                MyCanvas.Children.Add(ellipse);
+            }
+            Button.IsEnabled = false;
+            timer.Interval = TimeSpan.FromMilliseconds(1);
+            timer.Tick += Update!;
+            timer.Start();
+        }
+
+        private bool checkInitialCoordinates(Ball newball)
+        {
+            if (_balls.Count == 0)
+            {
+                return true;
+            }
+            for (int i = 0; i < _balls.Count; i++)
+            {
+                if (Math.Pow(newball.X - _balls[i].X, 2) + Math.Pow(newball.Y - _balls[i].Y, 2) <= Math.Pow(newball.R + _balls[i].R, 2))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        private void ResetBalls(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < MyCanvas.Children.Count; i++)
+            {
+                if (MyCanvas.Children[i] is Ellipse)
+                {
+                    MyCanvas.Children.RemoveAt(i);
+                    i--;
+                }
+            }
+            _balls.Clear();
+            Button.IsEnabled = true;
+        }
+
+        private void checkEdgeCollisions(Ball ball)
+        {
+            if (ball.Y > 411 - ball.R || ball.changeY)
+            {
+                ball.Y -= 1;
+                ball.changeY = true;
+            }
+            if (ball.Y < ball.R)
+            {
+                ball.Y += 1;
+                ball.changeY = false;
+            }
+            if (!ball.changeY)
+            {
+                ball.Y += 1;
+            }
+            if (ball.changeY)
+            {
+                ball.Y -= 1;
+            }
+            if (ball.X > 684 - ball.R || ball.changeX)
+            {
+                ball.X -= 1;
+                ball.changeX = true;
+            }
+            if (ball.X < ball.R)
+            {
+                ball.X += 1;
+                ball.changeX = false;
+            }
+            if (!ball.changeX)
+            {
+                ball.X += 1;
+            }
+            if (ball.changeX)
+            {
+                ball.X -= 1;
+            }
+        }
+        
+        private void checkBallCollisions(Ball ball1, Ball ball2) //prawie działa
+        {
+            if (Math.Pow(ball1.X - ball2.X, 2) + Math.Pow(ball1.Y - ball2.Y, 2) <= Math.Pow(ball1.R + ball2.R, 2))
+            {
+                ball1.changeX = !ball1.changeX;
+                ball1.changeY = !ball1.changeY;
+                ball2.changeX = !ball2.changeX;
+                ball2.changeY = !ball2.changeY;
+            }
+        }
+
+        private void Update(object sender, EventArgs e)
+        {
+            for (int i = 0; i < _balls.Count; i++)
+            {
+                checkEdgeCollisions(_balls[i]);
+                for (int j = 0; j < _balls.Count; j++)
+                {
+                    if (j != i)
+                    {
+                        checkBallCollisions(_balls[i], _balls[j]);
+                    }
+                }
+                Canvas.SetLeft(MyCanvas.Children[i + 1], _balls[i].X - _balls[i].R);
+                Canvas.SetTop(MyCanvas.Children[i + 1], _balls[i].Y - _balls[i].R);
+            }
+        }
+
+        private void StopBalls(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void StartBalls(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
         }
     }
 }
