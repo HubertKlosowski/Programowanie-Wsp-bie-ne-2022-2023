00ae3e08ebf828c441c5e7c2cd6b00688813a3b6
Hubert Kłosowski <242424@edu.p.lodz.pl>
2023-04-07 00:35:26 +0200

    Poprawione nazwy metod

--- Etap1/View/MainWindow.xaml.cs
+++ Etap1/View/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ namespace View
     public partial class MainWindow
     {
         private List<Ball> _balls = new();
-        private DispatcherTimer timer = new();
+        private DispatcherTimer _timer = new();
         
         public MainWindow()
         {
@@ -58,11 +58,11 @@ namespace View
                 int x = random.Next(5, (int)MyCanvas.ActualWidth - r);
                 int y = random.Next(5, (int)MyCanvas.ActualHeight - r);
                 Ball ball = new Ball(x, y, r);
-                while (!checkInitialCoordinates(ball))
+                while (!CheckInitialCoordinates(ball))
                 {
                     ball.X = random.Next(5, (int)MyCanvas.ActualWidth - r);
                     ball.Y= random.Next(5, (int)MyCanvas.ActualHeight - r);
-                    if (checkInitialCoordinates(ball))
+                    if (CheckInitialCoordinates(ball))
                     {
                         break;
                     }
@@ -79,12 +79,12 @@ namespace View
                 MyCanvas.Children.Add(ellipse);
             }
             Button.IsEnabled = false;
-            timer.Interval = TimeSpan.FromMilliseconds(1);
-            timer.Tick += Update!;
-            timer.Start();
+            _timer.Interval = TimeSpan.FromMilliseconds(1);
+            _timer.Tick += Update!;
+            _timer.Start();
         }
 
-        private bool checkInitialCoordinates(Ball newball)
+        private bool CheckInitialCoordinates(Ball newball)
         {
             if (_balls.Count == 0)
             {
@@ -116,7 +116,7 @@ namespace View
             Button.IsEnabled = true;
         }
 
-        private void checkEdgeCollisions(Ball ball)
+        private void CheckEdgeCollisions(Ball ball)
         {
             if (ball.Y > MyCanvas.ActualHeight - ball.R || ball.changeY)
             {
@@ -156,7 +156,7 @@ namespace View
             }
         }
         
-        private void checkBallCollisions(Ball ball1, Ball ball2)
+        private void CheckBallCollisions(Ball ball1, Ball ball2)
         {
             if (((ball1.X - ball2.X) * (ball1.X - ball2.X)) + ((ball1.Y - ball2.Y) * (ball1.Y - ball2.Y)) <= ((ball1.R + ball2.R) * (ball1.R + ball2.R)))
             {
@@ -180,10 +180,10 @@ namespace View
         {
             for (int i = 0; i < _balls.Count; i++)
             {
-                checkEdgeCollisions(_balls[i]);
+                CheckEdgeCollisions(_balls[i]);
                 for (int j = i + 1; j < _balls.Count; j++)
                 {
-                    checkBallCollisions(_balls[i], _balls[j]);
+                    CheckBallCollisions(_balls[i], _balls[j]);
                 }
                 Canvas.SetLeft(MyCanvas.Children[i + 1], _balls[i].X - _balls[i].R);
                 Canvas.SetTop(MyCanvas.Children[i + 1], _balls[i].Y - _balls[i].R);
@@ -192,12 +192,12 @@ namespace View
 
         private void StopBalls(object sender, RoutedEventArgs e)
         {
-            timer.Stop();
+            _timer.Stop();
         }
 
         private void StartBalls(object sender, RoutedEventArgs e)
         {
-            timer.Start();
+            _timer.Start();
         }
     }
 }
